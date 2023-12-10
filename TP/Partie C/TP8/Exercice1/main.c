223883b2f7cfad5257b6d8a2a2318671c2bb9c41
CEOS4C <68231770+CEOS4C@users.noreply.github.com>
2023-12-10 13:22:25 +0000

    Update main.c

--- TP/Partie C/TP8/Exercice1/main.c
+++ TP/Partie C/TP8/Exercice1/main.c
@@ -2,49 +2,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-#include <fcntl.h>
 
 #define NB_VAL 255
 
-void fusion(FILE *f, off_t debut, off_t milieu, off_t fin) {
-    int fd = fileno(f);
-
+void fusion(FILE *f, long debut, long milieu, long fin) {
     off_t n1 = milieu - debut + 1;
     off_t n2 = fin - milieu;
 
     unsigned long long *gauche = malloc(n1 * sizeof(unsigned long long));
     unsigned long long *droite = malloc(n2 * sizeof(unsigned long long));
 
-    lseek(fd, debut * sizeof(unsigned long long), SEEK_SET);
-    read(fd, gauche, n1 * sizeof(unsigned long long));
+    fseek(f, debut * sizeof(unsigned long long), SEEK_SET);
+    fread(gauche, sizeof(unsigned long long), n1, f);
 
-    lseek(fd, (milieu + 1) * sizeof(unsigned long long), SEEK_SET);
-    read(fd, droite, n2 * sizeof(unsigned long long));
+    fseek(f, (milieu + 1) * sizeof(unsigned long long), SEEK_SET);
+    fread(droite, sizeof(unsigned long long), n2, f);
 
-    off_t i = 0, j = 0, k = debut;
+    long i = 0, j = 0, k = debut;
     while (i < n1 && j < n2) {
         if (gauche[i] <= droite[j]) {
-            lseek(fd, k * sizeof(unsigned long long), SEEK_SET);
-            write(fd, &gauche[i], sizeof(unsigned long long));
+            fseek(f, k * sizeof(unsigned long long), SEEK_SET);
+            fwrite(&gauche[i], sizeof(unsigned long long), 1, f);
             i++;
         } else {
-            lseek(fd, k * sizeof(unsigned long long), SEEK_SET);
-            write(fd, &droite[j], sizeof(unsigned long long));
+            fseek(f, k * sizeof(unsigned long long), SEEK_SET);
+            fwrite(&droite[j], sizeof(unsigned long long), 1, f);
             j++;
         }
         k++;
     }
 
     while (i < n1) {
-        lseek(fd, k * sizeof(unsigned long long), SEEK_SET);
-        write(fd, &gauche[i], sizeof(unsigned long long));
+        fseek(f, k * sizeof(unsigned long long), SEEK_SET);
+        fwrite(&gauche[i], sizeof(unsigned long long), 1, f);
         i++;
         k++;
     }
 
     while (j < n2) {
-        lseek(fd, k * sizeof(unsigned long long), SEEK_SET);
-        write(fd, &droite[j], sizeof(unsigned long long));
+        fseek(f, k * sizeof(unsigned long long), SEEK_SET);
+        fwrite(&droite[j], sizeof(unsigned long long), 1, f);
         j++;
         k++;
     }
@@ -53,9 +50,9 @@ void fusion(FILE *f, off_t debut, off_t milieu, off_t fin) {
     free(droite);
 }
 
-void triFusion(FILE *f, off_t debut, off_t fin) {
+void triFusion(FILE *f, long debut, long fin) {
     if (debut < fin) {
-        off_t milieu = debut + (fin - debut) / 2;
+        long milieu = debut + (fin - debut) / 2;
 
         triFusion(f, debut, milieu);
         triFusion(f, milieu + 1, fin);
@@ -68,9 +65,9 @@ int main() {
     srand(time(NULL));
 
     unsigned long long nombre;
-    FILE *f = fopen("/home/ceos4c/Documents/UTBM/TC03/LP25/TP/Partie C/TP8/Exercice1/texte.txt", "r+b");
+    FILE *f = fopen("/home/ceos4c/Documents/UTBM/TC03/LP25/TP/Partie C/TP8/Exercice1/texte.txt", "w+b");
     if (f == NULL) {
-        perror("Erreur lors de l'ouverture du fichier texte.txt en lecture/écriture binaire");
+        perror("Erreur lors de l'ouverture du fichier en lecture/écriture binaire");
         return EXIT_FAILURE;
     }
 
